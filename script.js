286f1c9769b51b8f855c2af985f3b19f33b063f9
CharlesTheEgoist <charleswarroom@gmail.com>
2025-02-19 08:42:00 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     const buyButtons = document.querySelectorAll(".buy-btn");
 
     buyButtons.forEach(button => {
-        button.addEventListener("click", (e) => {
-            const product = e.target.dataset.product;
-            const price = e.target.dataset.price;
+        button.addEventListener("click", function () {
+            let productName = this.getAttribute("data-product");
+            let productPrice = this.getAttribute("data-price");
+
+            alert(`Added ${productName} to cart! Price: $${productPrice}`);
             
-            alert(`You are purchasing ${product} for $${price}. Redirecting to checkout...`);
-            
-            // Updated PayPal link
-            window.location.href = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=YOUR_PAYPAL_EMAIL&item_name=${product}&amount=${price}&currency_code=USD`;
+            // Redirect to PayPal Checkout
+            window.location.href = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=YOUR_PAYPAL_EMAIL&item_name=${productName}&amount=${productPrice}&currency_code=USD`;
         });
     });
 });
